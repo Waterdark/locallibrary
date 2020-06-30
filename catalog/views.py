969391aa8733bb9fd70a74cc8557b11789790ee0
Waterdark <shadow_priest@mail.ru>
2020-07-01 03:21:49 +0500

    changed comment into index function

--- catalog/views.py
+++ catalog/views.py
@@ -5,17 +5,15 @@ from django.views import generic
 # Create your views here.
 
 def index(request):
-    """
-    Функция отображения для домашней страницы сайта.
-    """
+    """Функция отображения для домашней страницы сайта."""
     # Генерация "количеств" некоторых главных объектов
     num_books = Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
     # Доступные книги (статус = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # Метод 'all()' применен по умолчанию.
     # Количество посещений этого представления, подсчитанное в переменной сеанса (session).
-    num_visits=request.session.get('num_visits', 0)
+    num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits+1
     # Отрисовка HTML-шаблона index.html с данными внутри 
     # переменной контекста context
