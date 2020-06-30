6710ee3444d5396d0cd710112fecece596ca3577
Waterdark <shadow_priest@mail.ru>
2020-07-01 01:22:50 +0500

    add new comment (path())

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,9 +6,12 @@ from django.conf.urls import url
 urlpatterns = [
     # path('', views.index, name='index'),
     url(r'^$', views.index, name='index'),
+    # path('books/', views.BookListView.as_view(), name='books'),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
+    # path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    # path('authors/', views.AuthorListView.as_view(), name='authors'),
     url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
-    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
     # path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
